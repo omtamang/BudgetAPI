7f93d38f2ce3dc520d4a86d114322fbb25b021e5
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2024-11-28 12:25:46 +0545

    First Commit

---
+++ src/main/java/com/umesh/budgetplanner/BudgetAPI/BudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.umesh.budgetplanner.BudgetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umesh/budgetplanner/BudgetAPI/BudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umesh.budgetplanner.BudgetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
